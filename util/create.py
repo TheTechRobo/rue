ec5c802a5c5d24d9b7eec386e0cc079eeb845a90
TheTechRobo <thetechrobo@proton.me>
2025-01-06 18:41:05 -0500

    Make creation script use argv instead of input()

--- util/create.py
+++ util/create.py
@@ -1,10 +1,13 @@
-import asyncio
+#!/usr/bin/env python3
+
+import asyncio, sys
 
 from rue import Queue, DefaultOptions
 
-database_name = input("Enter database name: ")
-print("Using default table names.")
-max_tries = int(input("Enter maxtries: "))
+if len(sys.argv) != 3:
+    sys.exit(f"Usage: {sys.argv[0]} <DB_NAME> <MAX_TRIES>")
+_, database_name, max_tries = sys.argv
+max_tries = int(max_tries)
 
 queue = Queue(database_name)
 res = asyncio.run(queue.create(DefaultOptions(max_tries = max_tries)))
